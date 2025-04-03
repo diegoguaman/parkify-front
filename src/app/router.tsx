140fe7e1b0b5ed30ab591cd42c422f5ac3b4f2a0
vlopez2828 <vickylopez2828@gmail.com>
2025-04-03 20:00:46 -0300

    Added route to registration page

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,7 +1,8 @@
 import { createBrowserRouter } from 'react-router-dom';
 import PublicLayout from '../layouts/PublicLayout';
 import HomePage from '../features/parkings/pages/HomePage';
-import RegisterPage, { action as registerAction } from '../features/auth/pages/RegisterPage';
+import RegisterPage from '../features/auth/pages/RegisterPage';
+
 
 export const router = createBrowserRouter([
   {
@@ -11,7 +12,6 @@ export const router = createBrowserRouter([
       { index: true, element: <HomePage /> },
       // { path: 'login', element: <LoginPage /> },
       { path: 'register',
-        action: registerAction, 
         element: <RegisterPage /> 
       },
     ],
