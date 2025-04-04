95686647c0cf0cd932f90fbfe3971330fb16ee68
Jesus Martin <jesus8.mb@gmail.com>
2025-04-04 14:37:06 +0200

    feature: header that changes according to the user.

--- src/shared/ui/Header.tsx
+++ src/shared/ui/Header.tsx
@@ -1,16 +1,20 @@
 import React, { useState } from "react";
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
 import IconButton from "@mui/material/IconButton";
+import LanguageOutlinedIcon from '@mui/icons-material/LanguageOutlined';
 import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined';
 import ManageAccountsOutlinedIcon from '@mui/icons-material/ManageAccountsOutlined';
 import Menu from "@mui/material/Menu";
 import MenuIcon from "@mui/icons-material/Menu";
 import MenuItem from "@mui/material/MenuItem";
 import NoCrashOutlinedIcon from '@mui/icons-material/NoCrashOutlined';
+import QuestionMarkOutlinedIcon from '@mui/icons-material/QuestionMarkOutlined';
+import ThreePOutlinedIcon from '@mui/icons-material/ThreePOutlined';
 import useMediaQuery from "@mui/material/useMediaQuery";
 import { useTheme } from "@mui/material/styles";
 
@@ -18,8 +22,53 @@ import Logo from "../../assets/react.svg"; // Change to new Logo path
 
 const Header: React.FC = () => {
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const [currentUserType, setCurrentUserType] = useState<'landing' | 'OwnerNotLogged' | 'OwnerLogged'>('landing');
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down("md"));
+
+  // Define the buttons for different user states
+  const authenticatedButton = [
+    {
+      user: "landing",
+      buttons: [
+        { label: "Inicio", icon: <HomeOutlinedIcon /> },
+        { label: "Panel de control", icon: <ManageAccountsOutlinedIcon /> },
+        { label: "Mis estacionamientos", icon: <NoCrashOutlinedIcon /> },
+        { label: "Salir", icon: <LogoutOutlinedIcon />, variant: "outlined" },
+      ],
+      menuIcon: <MenuIcon />,
+    },
+    {
+      user: "OwnerNotLogged",
+      buttons: [
+        { label: "Cómo funciona", icon: <QuestionMarkOutlinedIcon /> },
+        { label: "Reseña", icon: <ThreePOutlinedIcon /> },
+        { label: "Seguinos", icon: <LanguageOutlinedIcon /> }
+      ],
+      menuIcon: <MenuIcon />,
+    },
+    {
+      user: "OwnerLogged",
+      buttons: [
+        { label: "Mi cuenta", icon: <ManageAccountsOutlinedIcon /> },
+        { label: "Cerrar Sesión", icon: <LogoutOutlinedIcon />, color: "error.main" },
+      ],
+      menuIcon: <AccountCircleOutlinedIcon />,
+    },
+  ]
+  
+  // this is just for testing purposes remove it later !!!!!!!!!!!!
+  const testChangeUser = () => {
+    setCurrentUserType(prev => {
+      if (prev === 'landing') return 'OwnerNotLogged';
+      if (prev === 'OwnerNotLogged') return 'OwnerLogged';
+      return 'landing';
+    });
+  }
+  // this is just for testing purposes remove it later !!!!!!!!!!!!
+
+  // Get the current user configuration
+  const currentUserConfig = authenticatedButton.find(user => user.user === currentUserType) || authenticatedButton[0];
 
   // Handle menu open
   const handleMenuOpen = (event: React.MouseEvent<HTMLButtonElement>) => {
@@ -29,12 +78,7 @@ const Header: React.FC = () => {
   // Handle menu close
   const handleMenuClose = () => {
     setAnchorEl(null);
-  };
-
-  const iconStyles = {
-    display:"flex", 
-    alignItems: "center", 
-    marginY: 1
+    testChangeUser(); // this is just for testing purposes remove it later !!!!!!!!!!!!
   };
 
   return (
@@ -42,7 +86,7 @@ const Header: React.FC = () => {
     <Toolbar >
       {/* Logo */}
       <Box sx={{ flexGrow: 1 }}>
-        <img src={Logo} />
+        <img src={Logo} alt="Parkify logo"/>
       </Box>
 
       {/* Buttons */}
@@ -50,7 +94,7 @@ const Header: React.FC = () => {
         <>
         {/* Mobile version */}
           <IconButton color="inherit" onClick={handleMenuOpen}>
-            <MenuIcon />
+            {currentUserConfig.menuIcon}
           </IconButton>
 
           <Menu
@@ -64,33 +108,24 @@ const Header: React.FC = () => {
                 }
             }}
           >
-            <MenuItem onClick={handleMenuClose}>
-              <Box sx={{ display:"flex", alignItems: "center", marginY: 1 }}>
-                <HomeOutlinedIcon sx={{ marginRight: 1}}/>
-                Inicio
-              </Box>
-            </MenuItem>
-
-            <MenuItem onClick={handleMenuClose}>
-            <Box sx={{ display:"flex", alignItems: "center", marginY: 1 }}>
-              <ManageAccountsOutlinedIcon sx={{ marginRight: 1}}/>
-                Panel de control
-              </Box>
-            </MenuItem>
-
-            <MenuItem onClick={handleMenuClose}>
-              <Box sx={{ display:"flex", alignItems: "center", justifyContent: "center", marginY: 1 }}>
-                <NoCrashOutlinedIcon sx={{ marginRight: 1}}/> 
-                Mis estacionamientos
-              </Box>
-            </MenuItem>
-
-            <MenuItem onClick={handleMenuClose}>
-              <Box sx={{ display:"flex", alignItems: "center", marginY: 1,  }}>
-                <LogoutOutlinedIcon sx={{ marginRight: 1}}/> 
-                Salir
-              </Box>
-            </MenuItem>
+            { currentUserConfig.buttons.map((button) => (
+              <MenuItem 
+                key={button.label} 
+                onClick={handleMenuClose}
+                sx={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  width: "100%",
+                }}
+              >
+                <Box sx={{ display:"flex", alignItems: "center", justifyContent: "center", color: button.color || "inherit" }}>
+                <IconButton color="inherit" sx={{ marginRight: 1}}>
+                  {button.icon}
+                </IconButton>
+                {button.label}
+                </Box>
+              </MenuItem>
+            ))}
           </Menu>
         </>
 
@@ -99,10 +134,14 @@ const Header: React.FC = () => {
       ( 
         <Box>
           {/* Desktop version */}
-          <Button color="inherit">Inicio</Button>
-          <Button color="inherit">Panel de control</Button>
-          <Button color="inherit">Mis estacionamientos</Button>
-          <Button variant="outlined" color="inherit">Salir</Button>
+          { currentUserConfig.buttons.map((button) => (
+            <Button 
+              key={button.label}
+              variant={button.variant || "text"}
+            >
+              {button.label}
+            </Button>
+          ))}
         </Box>
       )
 
