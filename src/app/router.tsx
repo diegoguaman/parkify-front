bde035d9cb44e6720f7b10a19e0b7935f7049189
vlopez2828 <vickylopez2828@gmail.com>
2025-04-05 23:26:40 -0300

    changes in router

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -3,17 +3,34 @@ import PublicLayout from '../layouts/PublicLayout';
 import HomePage from '../features/parkings/pages/HomePage';
 import RegisterPage from '../features/auth/pages/RegisterPage';
 import LoginPage from '../features/auth/pages/LoginPage'
+import LayoutAuth from '../layouts/LayoutAuth';
 
 export const router = createBrowserRouter([
   {
-    path: '/',
+    element: <LayoutAuth />,
+    children: [
+      { path: "login", element: <LoginPage /> },
+      { path: "register", element: <RegisterPage /> },
+    ],
+  },
+
+  {
+    path: "/",
     element: <PublicLayout />,
     children: [
       { index: true, element: <HomePage /> },
-      { path: 'login', element: <LoginPage /> },
-      { path: 'register',
-        element: <RegisterPage /> 
-      },
+
     ],
   },
+  // {
+  //   path: '/',
+  //   element: <PublicLayout />,
+  //   children: [
+  //     { index: true, element: <HomePage /> },
+  //     { path: 'login', element: <LoginPage /> },
+  //     { path: 'register',
+  //       element: <RegisterPage /> 
+  //     },
+  //   ],
+  // },
 ]);
