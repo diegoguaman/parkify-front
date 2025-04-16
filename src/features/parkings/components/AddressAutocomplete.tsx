31bc186039202859c0ac05faabf04299fed93111
vlopez2828 <vickylopez2828@gmail.com>
2025-04-16 17:20:17 -0300

    feat: add prop and icon to AddressAutocomplete

--- src/features/parkings/components/AddressAutocomplete.tsx
+++ src/features/parkings/components/AddressAutocomplete.tsx
@@ -1,17 +1,18 @@
 import { Autocomplete } from '@react-google-maps/api';
-import { TextField } from '@mui/material';
+import { InputAdornment, TextField } from '@mui/material';
 import { useRef } from 'react';
 import { UseFormSetValue } from 'react-hook-form';
 import { useParkingStore } from '../../../store/parking.store';
-
+import SearchIcon from '@mui/icons-material/Search';
 interface Props {
-  setValue: UseFormSetValue<any>;
+  setValue?: UseFormSetValue<any>;
   placeholder?: string;
   error?: boolean;
   helperText?: string;
+  withSearchIcon?: boolean
 }
 
-export const AddressAutocomplete = ({ setValue, placeholder = "Dirección del estacionamiento", error, helperText }: Props) => {
+export const AddressAutocomplete = ({ setValue, placeholder = "Dirección del estacionamiento", error, helperText, withSearchIcon }: Props) => {
   const autocompleteRef = useRef<google.maps.places.Autocomplete | null>(null);
   const inputRef = useRef<HTMLInputElement | null>(null);
   const setParkingData = useParkingStore((state) => state.setParkingData)
@@ -26,7 +27,7 @@ export const AddressAutocomplete = ({ setValue, placeholder = "Dirección del es
       const lat = place.geometry.location.lat();
       const lng = place.geometry.location.lng();
 
-      setValue("parkingAddress", parkingAddress, {
+      setValue?.("parkingAddress", parkingAddress, {
         shouldValidate: true,
         shouldTouch: true,
       });
@@ -43,10 +44,18 @@ export const AddressAutocomplete = ({ setValue, placeholder = "Dirección del es
         inputRef={inputRef}
         fullWidth
         label={placeholder}
+        placeholder={placeholder}
         defaultValue={parkingData.parkingAddress || ""}
         error={error}
         helperText={helperText}
         sx={{width:"100%"}}
+        InputProps={{
+          startAdornment: withSearchIcon ? (
+            <InputAdornment position="start">
+              <SearchIcon sx={{ color: "grey.500" }} />
+            </InputAdornment>
+          ) : undefined,
+        }}
       />
     </Autocomplete>
   );
