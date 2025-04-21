dadbc39b6bc4b493a8cdf7c12002cf52defa8bb7
maurifigueroa <mauricio.david.figueroa@gmail.com>
2025-04-21 20:28:28 -0300

    modifico socket.ts para ver por que no habilita socket

--- src/features/parkings/lib/socket.ts
+++ src/features/parkings/lib/socket.ts
@@ -5,14 +5,24 @@ import { io, Socket } from 'socket.io-client'
 let socket: Socket | null = null
 
 export const getSocket = (): Socket | null => {
+  // Imprimir la variable de entorno para VITE_SOCKET_ENABLED
+  console.log('VITE_SOCKET_ENABLED:', import.meta.env.VITE_SOCKET_ENABLED);
+  
+  // Asegurarnos de que estamos comparando correctamente el valor
   const isSocketEnabled = import.meta.env.VITE_SOCKET_ENABLED === 'true';
+
+  // Imprimir si el socket está habilitado
+  console.log('Is WebSocket enabled?', isSocketEnabled);
 
   if (!isSocketEnabled) {
     console.warn('WebSocket connection disabled by environment configuration.');
     return null;
   }
+
+  // Si el socket aún no está instanciado, intentamos crearlo
   if (!socket) {
     try {
+      console.log('Trying to connect to socket with URL:', import.meta.env.VITE_API_URL);
       socket = io(import.meta.env.VITE_API_URL, {
         autoConnect: false,
         transports: ['websocket'],
@@ -22,5 +32,7 @@ export const getSocket = (): Socket | null => {
       socket = null;
     }
   }
+
+  // Devuelvo el socket
   return socket;
 }
