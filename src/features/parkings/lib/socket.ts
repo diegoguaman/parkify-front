f28a351af43c9c230191aceb6ae34397d1c23d40
maurifigueroa <mauricio.david.figueroa@gmail.com>
2025-04-22 21:20:07 -0300

    test modificando socket.ts

--- src/features/parkings/lib/socket.ts
+++ src/features/parkings/lib/socket.ts
@@ -6,12 +6,17 @@ export const getSocket = (): Socket | null => {
   const isSocketEnabled = import.meta.env.VITE_SOCKET_ENABLED?.toLowerCase() === 'true';
   const apiUrl = import.meta.env.VITE_API_URL;
 
+  console.log('VITE_SOCKET_ENABLED:', import.meta.env.VITE_SOCKET_ENABLED);
+  console.log('VITE_API_URL:', apiUrl);
+  console.log('Is WebSocket enabled?', isSocketEnabled);
+
   if (!isSocketEnabled) {
     console.warn('WebSocket connection disabled by environment configuration.');
     return null;
   }
 
   if (!socket) {
+    console.log('Connecting to WebSocket at URL:', apiUrl);
     socket = io(apiUrl, {
       autoConnect: false,
       transports: ['websocket'],
