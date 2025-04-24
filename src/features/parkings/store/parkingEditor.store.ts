b535cae04c351d9742132dafce1ccfb79c87b491
Diego Guaman <diegoguamanmedina@gmail.com>
2025-04-24 22:12:37 +0200

    comment unnecessary code

--- src/features/parkings/store/parkingEditor.store.ts
+++ src/features/parkings/store/parkingEditor.store.ts
@@ -1,34 +1,34 @@
-import { create } from 'zustand';
-import { Parking } from '../../../shared/types/parking';
+// import { create } from 'zustand';
+// import { Parking } from '../../../shared/types/parking';
 
-interface ParkingEditorStore extends Parking {
-  setParkingData: (data: Partial<Parking>) => void;
-  reset: () => void;
-}
+// interface ParkingEditorStore extends Parking {
+//   setParkingData: (data: Partial<Parking>) => void;
+//   reset: () => void;
+// }
 
-export const useParkingEditorStore = create<ParkingEditorStore>((set) => ({
-  id: '',
-  name: '',
-  address: '',
-  contactNumber: '',
-  hourlyRate: 0,
-  totalCapacity: 0,
-  availableSpots: 0,
-  lat: 0,
-  lng: 0,
-  distance: 0,
+// export const useParkingEditorStore = create<ParkingEditorStore>((set) => ({
+//   id: '',
+//   name: '',
+//   address: '',
+//   contactNumber: '',
+//   hourlyRate: 0,
+//   totalCapacity: 0,
+//   availableSpots: 0,
+//   lat: 0,
+//   lng: 0,
+//   distance: 0,
 
-  setParkingData: (data) => set((state) => ({ ...state, ...data })),
-  reset: () =>
-    set({
-      id: '',
-      name: '',
-      address: '',
-      contactNumber: '',
-      hourlyRate: 0,
-      totalCapacity: 0,
-      availableSpots: 0,
-      lat: 0,
-      lng: 0,
-    }),
-}));
+//   setParkingData: (data) => set((state) => ({ ...state, ...data })),
+//   reset: () =>
+//     set({
+//       id: '',
+//       name: '',
+//       address: '',
+//       contactNumber: '',
+//       hourlyRate: 0,
+//       totalCapacity: 0,
+//       availableSpots: 0,
+//       lat: 0,
+//       lng: 0,
+//     }),
+// }));
--- src/shared/types/parking.ts
+++ src/shared/types/parking.ts
@@ -1,18 +1,18 @@
-export interface Parking {
-  id: string;
-  name: string;
-  address: string;
-  contactNumber: string;
-  hourlyRate: number;
-  totalCapacity: number;
-  availableSpots: number;
-  lat: number;
-  lng: number;
-  imageParking?: string;
-  openTime?: string;
-  closeTime?: string;
-  rating?: number;
-  estimatedTime?: string;
-  distance: number; 
-  isOpen?: boolean;
-}
+// export interface Parking {
+//   id: string;
+//   name: string;
+//   address: string;
+//   contactNumber: string;
+//   hourlyRate: number;
+//   totalCapacity: number;
+//   availableSpots: number;
+//   lat: number;
+//   lng: number;
+//   imageParking?: string;
+//   openTime?: string;
+//   closeTime?: string;
+//   rating?: number;
+//   estimatedTime?: string;
+//   distance: number; 
+//   isOpen?: boolean;
+// }
