2ebe030fb4242763bf4476e245e74d314b98b5f8
vlopez2828 <vickylopez2828@gmail.com>
2025-05-08 15:41:26 -0300

    feat: add scroll-to-top on route change

--- src/layouts/PublicLayout.tsx
+++ src/layouts/PublicLayout.tsx
@@ -6,9 +6,11 @@ import Container from "@mui/material/Container";
 import Footer from '../shared/ui/Footer/Footer';
 import Header from "../shared/ui/Header";
 import { Box } from "@mui/material";
+import ScrollToTop from "../shared/ui/components/ScrollToTop";
 
 const PublicLayout = () => (
   <Box display="flex" flexDirection="column" minHeight="100vh">
+    <ScrollToTop />
     <Header />
     <Container disableGutters maxWidth={false} sx={{flexGrow: 1, p:0, mb:4}}>
       <Outlet />
